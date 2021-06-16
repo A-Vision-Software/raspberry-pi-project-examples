a84cb3ca4875cced75e68b67eac0d8ae3f0b3a7a
Arnold Velzel <info@a-vision.software>
2021-06-16 15:05:52 +0200

    Update devices.py

--- sensorbridge-thing/devices.py
+++ sensorbridge-thing/devices.py
@@ -10,13 +10,8 @@
 ################################################################################
 
 import smbus2
-import Adafruit_ADS1x15
-from sht20 import SHT20
 import time
 
-ADS1015ADDRESS = 0x48 # Alternatice address is 0x49
-SHT20ADDRESS = 0x40 # Fixed!
-SHT21ADDRESS = 0x40 # Fixed!
 TMP75ADDRESS = 0x4F # Fixed!
 MCP3425ADDRESS = 0x68 # Fixed!
 SENSORBRIDGEADDRESS = 0x70 # Range 0x70 - 0x77
@@ -78,97 +73,6 @@ class _TMP75():
             data = bus.read_i2c_block_data(self.address, 0x00, 2)
             bus.close()
             return (data[0] * 256 + data[1]) / 16 / 16
-        except: # exception if read_byte fails
-            return False
-################################################################################
-
-################################################################################
-class _ADS1015():
-
-    def __init__(self):
-        self.address = ADS1015ADDRESS
-        if self.available():
-            self.adc = Adafruit_ADS1x15.ADS1015(address=self.address)
-
-    def available(self):
-        return I2Cexists(self.address)
-
-    def read(self, port=0):
-        try:
-            if self.available():
-                return self.adc.read_adc(port, gain=1)
-            else:
-                return -1
-        except: # exception if read_byte fails
-            return False
-################################################################################
-
-################################################################################
-class _SHT20():
-
-    def __init__(self):
-        self.address = SHT20ADDRESS
-        self.sht20 = False
-
-    def available(self):
-        try:
-            if (self.sht20):
-                return True
-            self.sht20 =  SHT20(1, resolution=SHT20.TEMP_RES_14bit)
-            return True
-        except:
-            return False
-
-    def temperature(self):
-        try:
-            if self.available():
-                return self.sht20.read_temp()
-            else:
-                return -1
-        except:
-            return False
-
-    def humidity(self):
-        try:
-            if self.available():
-                return self.sht20.read_humid()
-            else:
-                return -1
-        except:
-            return False
-################################################################################
-
-################################################################################
-class _SHT21():
-
-    def __init__(self):
-        self.address = SHT21ADDRESS
-        self.sht21 = False
-
-    def available(self):
-        try:
-            if (self.sht21):
-                return True
-            self.sht21 =  SHT20(1, resolution=SHT20.TEMP_RES_14bit)
-            return True
-        except:
-            return False
-
-    def temperature(self):
-        try:
-            if self.available():
-                return self.sht21.read_temp()
-            else:
-                return -1
-        except: # exception if read_byte fails
-            return False
-
-    def humidity(self):
-        try:
-            if self.available():
-                return self.sht21.read_humid()
-            else:
-                return -1
         except: # exception if read_byte fails
             return False
 ################################################################################
