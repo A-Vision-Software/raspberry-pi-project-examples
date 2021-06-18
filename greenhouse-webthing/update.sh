1a99cc4a2baad826c1c281cf7437fe31a78720ff
Arnold Velzel <info@a-vision.software>
2021-06-18 16:52:33 +0200

    Update update.sh

--- greenhouse-webthing/update.sh
+++ greenhouse-webthing/update.sh
@@ -4,8 +4,8 @@ echo "updating source code"
 wget https://github.com/A-Vision-Software/raspberry-pi-project-examples/archive/refs/heads/main.zip
 unzip -o main.zip "raspberry-pi-project-examples-main/greenhouse-webthing/*" -d .
 rm main.zip
-mv raspberry-pi-project-examples-main/greenhouse-webthing/* .
-rm -r raspberry-pi-project-examples-main/
+cp -rf raspberry-pi-project-examples-main/greenhouse-webthing/* .
+rm -r raspberry-pi-project-examples-main
 
 chmod 0755 install.sh
 chmod 0755 update.sh
